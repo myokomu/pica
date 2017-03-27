be13d40aeb4c023f85c97627411e6bf828affb49
Steven Wang <3460883@gmail.com>
2017-03-27 19:04:08 +0800

    implement canvas with react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import Canvas from './Canvas'
 import './App.css'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      rows: "16",
+      columns: "16"
+    }
+  }
+
   render() {
     return(
       <div>
-        Hello World
+        <Canvas rows={this.state.rows} columns={this.state.columns} />
       </div>
     )
   }
---
+++ src/Canvas.js
@@ -0,0 +1,24 @@
+import _ from 'lodash'
+import React, {Component} from 'react'
+
+class Canvas extends Component {
+  render () {
+    return (
+      <div>
+        {_.times(this.props.rows, (i) => {
+          return(
+            <div className={i%2 === 0 ? "odd row" : "even row"} key={i}>
+              {_.times(this.props.columns, (j) => {
+                return(
+                  <div className={j%2 === 0 ? "odd column" : "even column"} key={j}>
+                  </div>
+                )
+              })}
+            </div>)
+        })}
+      </div>
+    )
+  }
+}
+
+export default Canvas
