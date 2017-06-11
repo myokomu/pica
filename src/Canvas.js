16e9f5e88231e9c0f6497789508eb05198733f65
Steven Wang <3460883@gmail.com>
2017-06-11 00:18:33 -0700

    add class name to canvas

--- src/Canvas.js
+++ src/Canvas.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react'
 class Canvas extends Component {
   render () {
     return (
-      <div>
+      <div className='canvas'>
         {_.times(this.props.rows, (i) => {
           return(
             <div className={i%2 === 0 ? "odd row" : "even row"} key={i}>
